2c0ee0c37ee1e26e97c388ff101a2b68db5054ea
Alper <akcanalpermutlu@gmail.com>
2025-06-21 02:18:02 +0300

    Gives handleClick logic for Card component's onClick prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import './App.css'
 import pokemonNames from './data/pokemon-names.json'
 import Card from './components/Card'
+import { useState } from 'react'
 
 function App() {
+  const [clickedCards, setClickedCards] = useState(new Array(12).fill(false))
+  const [streak, setStreak] = useState(0)
+
+  function handleClick(e) {
+    e.stopPropagation()
+    const clickedPokemon = e.currentTarget.childNodes[1].innerHTML
+
+    const pokemonIndex = pokemonNames.find((pokemon) => pokemon.name == clickedPokemon).id
+
+    setClickedCards(
+      clickedCards.map((card, i) => {
+        return i == pokemonIndex ? true : card
+      })
+    )
+  }
+
+  console.log(clickedCards)
+
   return (
-    <main>
-      {pokemonNames.map((pokemon) => (
-        <Card key={pokemon.id} name={pokemon.name} />
-      ))}
-    </main>
+    <>
+      <header>Score: </header>
+      <main>
+        {pokemonNames.map((pokemon) => (
+          <Card key={pokemon.id} name={pokemon.name} onClick={handleClick} />
+        ))}
+      </main>
+    </>
   )
 }
 
