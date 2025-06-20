eee2e95437c7fa17b9ca3fb66159426364898057
Alper <akcanalpermutlu@gmail.com>
2025-06-21 02:15:52 +0300

    Updates Card component props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import '../styles/Card.css'
 import { usePokemon } from '../hooks/usePokemon'
 
-function Card({ name }) {
+function Card({ name, onClick }) {
   const { pokemon, error, loading } = usePokemon(name)
 
-  console.log(pokemon)
+  //console.log(pokemon)
 
   if (loading) return <div>Loading...</div>
 
   if (error) return <div>Oops, something went wrong!</div>
 
   return (
-    <div className="game-card">
-      <img src="" alt="" />
-      <h2>{pokemon.name}</h2>
+    <div className="game-card" onClick={onClick}>
+      <img src="hello" alt="" />
+      <h2 >{pokemon.name}</h2>
     </div>
   )
 }
